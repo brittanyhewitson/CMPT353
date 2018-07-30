23950cdb3dcac15e40d7cc778c4c0fc692f3c065
brittanyhewitson <britt.hewitson@gmail.com>
2018-07-30 16:08:35 -0700

    stuff

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
+from math import sqrt
+import re
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import FunctionTransformer
 from sklearn.pipeline import make_pipeline
@@ -54,11 +56,11 @@ def plot_gForce(df, x_axis):
 
 def plot_acc(df, x_axis):
     plt.plot(df[x_axis], df['acceleration'])
-    plt.show()
+    #plt.show()
 
 def plot_vel(df, x_axis):
     plt.plot(df[x_axis], df['velocity'])
-    plt.show()
+    #plt.show()
 
 def eucl_dist_w(df):
     return sqrt(df['wx']**2 + df['wy']**2 + df['wz']**2)
@@ -71,6 +73,10 @@ def main():
     names = ['1_left', '1_right', '2_left', '2_right', '4_left', '4_right', '5_left', '5_right', '6_left', \
             '6_right', '7_left', '7_right', '8_left', '8_right']
 
+    data_sum = pd.read_csv('Data_Summary.csv')
+    data_sum['freq'] = ''
+    data_sum = data_sum.set_index('F_name')
+    
     sensor_data = {}
 
     for i in range(len(names)):
@@ -83,7 +89,6 @@ def main():
         walk_data['acceleration'] = temp.apply(eucl_dist_a, axis=1)
         walk_data['velocity'] = temp.apply(eucl_dist_w, axis=1)
         
-
         #Filter the data
         data_filt = walk_data.apply(filter_df, axis=0)
 
@@ -106,12 +111,17 @@ def main():
         max_ind = ind.idxmax()
         avg_freq = data_FT.at[max_ind, 'freq']
 
+        #Store into the main dataframe
+        data_sum.at[names[i], 'freq'] = avg_freq
+
         #Store in the dictionary
         str_filt = names[i] + '_filt'
         str_FT = names[i] + '_FT'
 
         sensor_data[str_filt] = data_filt
         sensor_data[str_FT] = data_FT
+
+    print(data_sum)
 
 
 if __name__=='__main__':
